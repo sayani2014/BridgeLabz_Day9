7a851ad77e3dd890a0f69c634fc1277571686def
sayani2014 <sayanikoley8@gmail.com>
2021-05-25 21:00:32 +0530

    Calculate the wages for a month with total working hours or days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,21 +3,33 @@
 wagePerHour=20
 fullDayHour=8
 partTime=4
-dailyWage=$((wagePerHour * fullDayHour * 20))
-partTimeWage=$((wagePerHour * partTime * 20))
+read -p "Enter the total working hours: " totalWorkingHours
+read -p "Enter the total working days: " totalDays
+dailyWage=$((wagePerHour * 100 * 20))
+partTimeWage=$((wagePerHour * 100 * 20 / 2))
 employeeAttendance=$((RANDOM%2+1))
 
 case $employeeAttendance in
 	1)
-		echo "Employee is present"
-		echo "The employee's salary for the current day is Rs.$dailyWage"	
+		echo "Employee is present today"
+		if(($totalWorkingHours>=100 || $totalDays==20))
+		then
+			echo "The employee's salary for the month is Rs.$dailyWage"
+		else
+			echo "Working limits not completed"
+		fi	
 	;;
 	2)
-		echo "Employee is having part time duty"
-		echo "The employee's salary for the current day is Rs.$partTimeWage"	
+		echo "Employee is having part time duty today"
+		if(($totalWorkingHours>=100 || $totalDays==20))
+		then
+			echo "The employee's salary for the month is Rs.$partTimeWage"
+		else
+			echo "Working limits not completed"
+		fi			
 	;;
 	0)
 		echo "Employee is absent"
-		echo "The employee's salary for the current day is Rs.0"
+		echo "The employee's salary for the month is Rs.0"
 esac
 
