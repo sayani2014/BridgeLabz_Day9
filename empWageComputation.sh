5126132d9b53d9a6dd7be85d8484c2163406fd0d
sayani2014 <sayanikoley8@gmail.com>
2021-05-25 20:14:24 +0530

    Check the daily wage of the employee UC

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,10 +2,15 @@
 
 echo "Welcome to Employee Wage Computation Program"
 
+wagePerHour=20
+fullDayHour=8
+dailyWage=$((wagePerHour * fullDayHour))
 employeeAttendance=$((RANDOM%2))
 if(($employeeAttendance==1))
 then
 	echo "Employee is present"
+	echo "The employee's salary for the current day is Rs.$dailyWage"
 else
 	echo "Employee is absent"
+	echo "The employee's salary for the current day is Rs.0"
 fi
