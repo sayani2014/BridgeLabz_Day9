7ed0eec69c66230b59f95a6a28b57a0b5702d33e
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-05-25 21:36:48 +0530

    updated

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,18 +3,29 @@
 wagePerHour=20
 fullDayHour=8
 partTime=4
+hoursPerDay=0
 read -p "Enter the total working hours: " totalWorkingHours
 read -p "Enter the total working days: " totalDays
-totaldailyWage=$((wagePerHour * 100 * 20))
-totalpartTimeWage=$((wagePerHour * 100 * 20 / 2))
+dailyWage=$((wagePerHour * 100 * 20))
+partTimeWage=$((wagePerHour * 100 * 20 / 2))
 employeeAttendance=$((RANDOM%2+1))
+
+workingHoursFulltime () {
+	hoursPerDay=$((totalWorkingHours / totalDays))
+	echo "Work hours per day: $hoursPerDay"
+}
+workingHoursParttime () {
+	hoursPerDay=$((totalWorkingHours / totalDays / 2))
+	echo "Work hours per day: $hoursPerDay"
+}
 
 case $employeeAttendance in
 	1)
 		echo "Employee is present today"
 		if(($totalWorkingHours>=100 || $totalDays==20))
 		then
-			echo "The employee's salary for the month is Rs.$totaldailyWage"
+			echo "The employee's salary for the month is Rs.$dailyWage"
+			workingHoursFulltime
 		else
 			echo "Working limits not completed"
 		fi	
@@ -23,7 +34,8 @@ case $employeeAttendance in
 		echo "Employee is having part time duty today"
 		if(($totalWorkingHours>=100 || $totalDays==20))
 		then
-			echo "The employee's salary for the month is Rs.$totalpartTimeWage"
+			echo "The employee's salary for the month is Rs.$partTimeWage"
+			workingHoursParttime
 		else
 			echo "Working limits not completed"
 		fi			
