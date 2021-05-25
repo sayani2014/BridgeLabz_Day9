be933b0466ae194575c88e69b135521ec7fdd028
sayani2014 <sayanikoley8@gmail.com>
2021-05-25 20:27:57 +0530

    Check the part time wage of the employee UC

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,15 +1,19 @@
 #! /bin/bash -x
 
-echo "Welcome to Employee Wage Computation Program"
-
 wagePerHour=20
 fullDayHour=8
+partTime=4
 dailyWage=$((wagePerHour * fullDayHour))
-employeeAttendance=$((RANDOM%2))
+partTimeWage=$((wagePerHour * partTime))
+employeeAttendance=$((RANDOM%2+1))
 if(($employeeAttendance==1))
 then
 	echo "Employee is present"
 	echo "The employee's salary for the current day is Rs.$dailyWage"
+elif(($employeeAttendance==2))
+then
+	echo "Employee is having part time duty"
+	echo "The employee's salary for the current day is Rs.$partTimeWage"
 else
 	echo "Employee is absent"
 	echo "The employee's salary for the current day is Rs.0"
